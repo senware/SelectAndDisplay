c1a3e5aac8f32d74edfe3475153c2ed086af9326
Sen <tug81999@temple.edu>
2021-03-02 21:25:35 -0500

    renamed activities, added imageview to display activity, added default selection to spinner

--- app/src/main/java/edu/temple/selectanddisplay/DisplayActivity.java
+++ app/src/main/java/edu/temple/selectanddisplay/DisplayActivity.java
@@ -16,5 +16,7 @@ public class DisplayActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display);
+
+        getSupportActionBar().setTitle("Display");
     }
 }
--- app/src/main/java/edu/temple/selectanddisplay/SelectionActivity.java
+++ app/src/main/java/edu/temple/selectanddisplay/SelectionActivity.java
@@ -2,21 +2,27 @@ package edu.temple.selectanddisplay;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Spinner;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+public class SelectionActivity extends AppCompatActivity {
 
     Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_selection);
+
+        getSupportActionBar().setTitle("Selection");
 
         ArrayList<Dino> dinoArrayList = new ArrayList<>();
+        dinoArrayList.add(new Dino("Choose a dinosaur", 0));
         dinoArrayList.add(new Dino("Triceratops", R.drawable.triceratops));
         dinoArrayList.add(new Dino("Stegosaurus", R.drawable.stegosaurus));
         dinoArrayList.add(new Dino("Utahraptor", R.drawable.raptor));
@@ -28,5 +34,21 @@ public class MainActivity extends AppCompatActivity {
 
         spinner = findViewById((R.id.dinoSpinner));
         spinner.setAdapter(dinoAdapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if (position == 0) {
+                    return;
+                } else {
+
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
 }
