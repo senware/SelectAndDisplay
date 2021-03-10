7bae34aec3ce0132d3eeb0e3c5bb4650606bb5bf
Sen <tug81999@temple.edu>
2021-03-09 19:28:04 -0500

    added all the formatting

--- app/src/main/java/edu/temple/selectanddisplay/DinoAdapter.java
+++ app/src/main/java/edu/temple/selectanddisplay/DinoAdapter.java
@@ -1,12 +1,16 @@
 package edu.temple.selectanddisplay;
 
+import android.app.ActionBar;
 import android.content.Context;
+import android.text.Layout;
+import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
@@ -46,67 +50,83 @@ public class DinoAdapter extends BaseAdapter {
     @Override
     // the non-dropdown view only displays the name
     public View getView(int position, View convertView, ViewGroup parent) {
-        FrameLayout frameLayout;
+        RelativeLayout relativeLayout;
         ImageView imageView;
         TextView textView;
 
         if (convertView == null) {
-            frameLayout = new FrameLayout(context);
+            relativeLayout = new RelativeLayout(context);
             imageView = new ImageView(context);
             textView = new TextView(context);
-            frameLayout.addView(imageView);
-            frameLayout.addView(textView);
-            textView.setTextSize(20);
-            textView.setTextColor(0x000000);
-            imageView.getLayoutParams().height = 300;
-            imageView.getLayoutParams().width = 300;
+
+            relativeLayout.addView(imageView);
+            relativeLayout.addView(textView);
+
+            // put the text at the bottom
+            RelativeLayout.LayoutParams textParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+            textParams.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM, RelativeLayout.TRUE);
+
+            // text view formatting
+            textView.setLayoutParams(textParams);
+            textView.setTextSize(14);
+            textView.setTextColor(0xFFFFFFFF);
+            textView.setGravity(Gravity.CENTER_HORIZONTAL);
+            textView.setBackgroundResource(R.color.grey);
+
+            // image view formatting
+            imageView.getLayoutParams().height = 400;
+            imageView.getLayoutParams().width = 400;
+
         } else {
-            imageView = (ImageView) convertView;
+            relativeLayout = (RelativeLayout) convertView;
+            imageView = (ImageView) relativeLayout.getChildAt(0);
+            textView = (TextView) relativeLayout.getChildAt((1));
         }
 
-        imageView.setImageResource(items.get(position).getImageRes());
-
-        return imageView;
-    }
-
-    @Override
-    // the dropdown view displays both the name and the image
-    public View getDropDownView(int position, View convertView, ViewGroup parent) {
-        TextView textView;
-        ImageView imageView;
-
-        // container for object data views
-        LinearLayout linearLayout;
-
-        // only need to do this once for entire array
-        if (convertView == null) {
-            linearLayout = new LinearLayout(context);
-            linearLayout.setOrientation((LinearLayout.HORIZONTAL));
-
-            textView = new TextView(context);
-            imageView = new ImageView(context);
-
-            textView.setPadding(8, 16, 16, 16);
-            textView.setTextSize(20);
-
-            linearLayout.addView(imageView);
-            linearLayout.addView(textView);
-
-            imageView.getLayoutParams().height = 150;
-            imageView.getLayoutParams().width = 150;
-        }
-        // retrieve views formatted as above
-        else {
-            linearLayout = (LinearLayout) convertView;
-            imageView = (ImageView) linearLayout.getChildAt(0);
-            textView = (TextView) linearLayout.getChildAt((1));
-        }
-
-        // necessarily unique per array element
-        // do this for each of them
         imageView.setImageResource(items.get(position).getImageRes());
         textView.setText(items.get(position).getName());
 
-        return linearLayout;
+        return relativeLayout;
     }
+
+//    @Override
+//    // the dropdown view displays both the name and the image
+//    public View getDropDownView(int position, View convertView, ViewGroup parent) {
+//        TextView textView;
+//        ImageView imageView;
+//
+//        // container for object data views
+//        LinearLayout linearLayout;
+//
+//        // only need to do this once for entire array
+//        if (convertView == null) {
+//            linearLayout = new LinearLayout(context);
+//            linearLayout.setOrientation((LinearLayout.HORIZONTAL));
+//
+//            textView = new TextView(context);
+//            imageView = new ImageView(context);
+//
+//            textView.setPadding(8, 16, 16, 16);
+//            textView.setTextSize(20);
+//
+//            linearLayout.addView(imageView);
+//            linearLayout.addView(textView);
+//
+//            imageView.getLayoutParams().height = 150;
+//            imageView.getLayoutParams().width = 150;
+//        }
+//        // retrieve views formatted as above
+//        else {
+//            linearLayout = (LinearLayout) convertView;
+//            imageView = (ImageView) linearLayout.getChildAt(0);
+//            textView = (TextView) linearLayout.getChildAt((1));
+//        }
+//
+//        // necessarily unique per array element
+//        // do this for each of them
+//        imageView.setImageResource(items.get(position).getImageRes());
+//        textView.setText(items.get(position).getName());
+//
+//        return linearLayout;
+//    }
 }
